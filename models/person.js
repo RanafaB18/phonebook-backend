78745362a1187d6a76eebd70d2a01024bc0b13df
Baba Abdul-Raziq <raziqbaba321@gmail.com>
2023-02-18 16:40:33 +0000

    Logging

--- models/person.js
+++ models/person.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.set("strictQuery", false);
+console.log(`Connecting to ${process.env.MONGODB_URI}`)
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((result) => {
